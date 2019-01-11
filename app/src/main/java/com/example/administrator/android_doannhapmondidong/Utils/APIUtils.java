ea8dd62b483caed8fd6a8f794ae9647e7f54db62
hoango7604 <30996727+hoango7604@users.noreply.github.com>
2019-01-11 16:43:09 +0700

    update

--- app/src/main/java/com/example/administrator/android_doannhapmondidong/Utils/APIUtils.java
+++ app/src/main/java/com/example/administrator/android_doannhapmondidong/Utils/APIUtils.java
@@ -5,7 +5,7 @@ import com.example.administrator.android_doannhapmondidong.Services.APIService;
 import retrofit2.Retrofit;
 
 public class APIUtils {
-    private static final String BASE_URL = "http://192.168.31.9/server/";
+    private static final String BASE_URL = "http://doannhapmongame.000webhostapp.com/server/";
 
     public static APIService getAPIService(){
         return RetrofitClient.getClient(BASE_URL).create(APIService.class);
